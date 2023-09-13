e77c98887e41d0ec5322de9bae07c30465962313
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2023-09-13 14:04:07 -0700

    practiced adding keydown and change events
    Used keydown events to change the website style from dark mode to light mode when the letter "L" is pressed, and back to dark mode when the letter "D" is pressed.
    
    Used change events to change the h1 element based on the user selection from the drop-down list.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
+//Keydown Events
+var body = document.querySelector("body");
 
+document.addEventListener("keydown", function (e) {
+  console.log(e);
+  if (e.key === "l") {
+    body.classList.add("light");
+  } else if (e.key === "d") {
+    if (body.classList.contains("light")) {
+      body.classList.remove("light");
+    }
+  }
+});
+
+//Change events
+var fave = document.querySelector("#favorite");
+var heading = document.querySelector("h1");
+var selection = "regular";
+
+fave.addEventListener("change", function (e) {
+  selection = e.target.value;
+  if (selection === "stealth") {
+    heading.innerText = "Stealthy Quincy 😎";
+  } else if (selection === "party") {
+    heading.innerText = "Party Quincy 🥳";
+  } else {
+    heading.innerText = "Quincy";
+  }
+});
